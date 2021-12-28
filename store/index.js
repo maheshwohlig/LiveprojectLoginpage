13f507065456ea535366e8b16f026c47491bf8e8
maheshwohlig <maheshwohlig@gmail.com>
2021-12-28 14:50:55 +0530

    vuexclear

--- store/index.js
+++ store/index.js
@@ -15,16 +15,18 @@ export const state = () => ({
     //actions
     actions = {
 
-        async fetchdata(username, password) {
-            let url = 'https://users.zodexchange.com';
+        async fetchdata({ commit }, { username, password }) {
+            let url = 'https://users.zodexchange.com'
             await axios.post(url + `/api/member/playerLogin`, {
                 username, password, siteOrigin: 'zodexchange.com',
             })
                 .then((response) => {
+                    console.log(response)
                     let v = response.data.value
-                    let user_name = JSON.parse(response.config.data)
-                    localStorage.setItem('username-info', user_name.username)
-                    localStorage.setItem('password-info', user_name.password)
+                    let st_data = response.data.data
+                    console.log("????", st_data)
+                    localStorage.setItem('username-info', JSON.stringify(st_data))
+                    // localStorage.setItem('password-info', user_name.password)
                     if (v === true) {
                         this.$router.push('/home')
                     }
